4190bfa955907d93bee6530a9c8df881528776e7
syOpt <xzcmz828@126.com>
2017-07-17 20:11:11 +0800

    PageImage类的函数实现
    实现了部分PageImage类的功能

--- PageDewarp/PageDewarp/PageImage.cpp
+++ PageDewarp/PageDewarp/PageImage.cpp
@@ -9,13 +9,9 @@ PageImage::PageImage() {
 PageImage::PageImage(const char *fileName) {
 	/* 用图片文件名初始化PageImage
 	*/
+	setFileInfo(fileName);
 	img = new Mat(imread(fileName)); // 从文件中读入图像
-	if (img->empty()) {
-		throw runtime_error(strcat("Can not load image: ", fileName)); // 如果读入图像失败则抛出异常
-	}	
-	width = img->cols;
-	height = img->rows;
-	bound = Boundary(0, 0, width, height);
+	setImageInfo();
 }
 
 PageImage::~PageImage() {
@@ -24,9 +20,172 @@ PageImage::~PageImage() {
 	delete img;
 }
 
+void PageImage::setImage(const Mat &m) {
+	/* 用一个Mat对象设置PageImage对象保存的图片。
+	PageImage对象丢弃现有的数据，保存传入的Mat的副本，
+	并初始化类内参数
+	*/
+	if (img != nullptr) delete img;
+	setFileInfo(nullptr);
+	img = new Mat(m);
+	setImageInfo();
+}
+
+void PageImage::setImage(const char *fileName) {
+	/* 用一个文件名设置PageImage对象保存的图片。
+	PageImage对象丢弃现有的数据，以cv::Mat格式保存传入的图像文件，
+	并初始化类内参数
+	*/
+	if (img != nullptr) delete img;
+	setFileInfo(fileName);
+	img = new Mat(imread(fileName));
+	setImageInfo();
+}
+
+void PageImage::setBoundary(int a, int b, int c, int d) {
+	/* 设置图像的活动区域
+	*/ 
+	bound = Boundary(a, b, c, d);
+}
+
+pair<int, int> PageImage::getSize() {
+	/* 返回图像活动区域的大小
+	*/
+	return pair<int, int>(bound.x2 - bound.x1 + 1, bound.y2 - bound.y1 + 1);
+}
+
+pair<int, int> PageImage::getSizeActual() {
+	/* 返回图像的实际大小
+	*/
+	return pair<int, int>(width, height);
+}
+
 void PageImage::showImage() {
 	/* 显示图像
 	*/
 	imshow("image", *img); 
 	waitKey(); //此函数等待按键，按键盘任意键就返回
+}
+
+void PageImage::saveImage() {
+	/* 使用默认文件名和类型保存图像
+	*/
+	doSave(fileDirAndName + '.' + fileType);
+}
+
+void PageImage::saveImage(const char *f) {
+	/* 指定文件名和类型保存图像
+	*/
+	doSave(f);
+}
+
+const uchar * PageImage::getRow(const int &r) {
+	/* 返回指定行的头指针
+	*/
+	return img->ptr<uchar>(r);
+}
+
+const uchar PageImage::getPixel(const int &r, const int &c) {
+	/* 返回指定行列的像素值
+	*/
+	return img->ptr<uchar>(r)[c];
+}
+
+void PageImage::setPixel(const int &r, const int &c, const uchar &value) {
+	/* 设置指定行列的像素值
+	*/
+	img->ptr<uchar>(r)[c] = value;
+}
+
+const uchar* PageImage::vProject() {
+	/* 将图像在竖直方向上投影，警告：若使用完成后不delete返回的指针将造成内存泄漏
+	*/
+	int sz = getSize().first;
+	uchar *ret = new uchar[sz];
+
+	for (int i = bound.y1; i <= bound.y2; ++i) {
+		uchar *p = img->ptr<uchar>(i);
+		int j = 0, col = j + bound.x1;
+		while (j < sz) {
+			ret[j++] += p[col++];
+		}
+	}
+
+	return ret;
+}
+
+const uchar * PageImage::hProject() {
+	/* 将图像在水平方向上投影，警告：若使用完成后不delete返回的指针将造成内存泄漏
+	*/
+	int sz = getSize().second;
+	uchar *ret = new uchar[sz];
+
+	int i = 0, row = i + bound.y1;
+	while (i < sz) {
+		uchar *p = img->ptr<uchar>(row++);
+		for (int j = bound.x1; j <= bound.x2; ++j) {
+			ret[i] += p[j];
+		}
+		++i;
+	}
+
+	return ret;
+}
+
+void PageImage::grayScale() {
+	/* 将图像灰度化。使用cvtColor
+	*/
+	Mat *newImg = new Mat(height, width, CV_8UC1);
+	cvtColor(*img, *newImg, CV_RGB2GRAY);
+	delete img;
+	img = newImg;
+}
+
+void PageImage::binary(const int &thre) {
+	/* 将图像二值化。使用cvThreshold
+	*/
+	//////////////////////////////FixMe/////////////////////////////
+	cvThreshold(img, img, 300, 255, CV_THRESH_OTSU | CV_THRESH_BINARY_INV);
+}
+
+void PageImage::setFileInfo(const char *fileName) {
+	/* 私有函数，设置文件名、文件类型信息
+	*/
+	bool findDot = false;
+	// 一次遍历输入的字符串，将‘.’之前的视作文件路径和文件名，之后的视为文件类型
+	while (fileName++) {
+		if (!findDot) {
+			if (*fileName == '.') {
+				findDot = true;
+			} else {
+				fileDirAndName += *fileName;
+			}
+		} else {
+			fileType += *fileName;
+		}
+	}
+	// 对非常规的输入做特殊处理
+	if (fileDirAndName.empty()) {
+		fileDirAndName = "unnamed";
+	}
+	if (fileType.empty()) {
+		fileType = "unspecified_type";
+	}
+}
+
+void PageImage::setImageInfo() {
+	/* 私有函数，设置图像的实际长、宽和边界
+	*/
+	if (img->empty()) {
+		throw runtime_error("Can not load image: " + fileDirAndName + "." + fileType); // 如果读入图像失败则抛出异常
+	}
+	width = img->cols;
+	height = img->rows;
+	bound = Boundary(0, 0, width - 1, height - 1);
+}
+
+void PageImage::doSave(const string &fileName) {
+	/* 私有函数，保存图像到文件
+	*/
+	imwrite(fileName, *img);
 }
--- PageDewarp/PageDewarp/PageImage.h
+++ PageDewarp/PageDewarp/PageImage.h
@@ -3,7 +3,7 @@ PageImage类
 封装图片相关的信息和操作，隐藏图片的数据结构细节，为矫正算法提供所需的图片接口，
 若后续改用opencv以外的库，只需要在这个类里作修改即可，矫正算法就无需调整了。
 1.提供图片信息，包括图片本身和图片类型
-2.提供图片操作，包括提取像素、投影统计、裁剪、灰度化、画质增强、二值化、膨胀、插值、变形等
+2.提供图片操作，包括提取像素、投影统计、灰度化、画质增强、二值化、膨胀、插值、变形等
 */
 
 #pragma once
@@ -24,14 +24,15 @@ public:
 	void setBoundary(int, int, int, int);
 	pair<int, int> getSize();
 	pair<int, int> getSizeActual();
-	Mat getImage();
 	void showImage();
+	void saveImage();
+	void saveImage(const char *);
 
-	vector<uint> *getColumn(const int &);
-	vector<uint> *getRow(const int &);
-	vector<uint> *vProject();
-	vector<uint> *hProject();
-	void trim(const int &, const int &, const int &, const int &);
+	const uchar* getRow(const int &);
+	const uchar getPixel(const int &, const int &);
+	void setPixel(const int &, const int &, const uchar &);
+	const uchar* vProject();
+	const uchar* hProject();
 	void grayScale();
 	void enhancement(int(*)(int));
 	void binary(const int &);
@@ -48,9 +49,14 @@ public:
 	};
 
 private:
-	Mat *img;  // 用于保存图片的变量
+	Mat *img;  // 保存图片
+	string fileDirAndName;  // 记录图片存储位置和文件名
+	string fileType; // 记录文件类型
 	int width, height;   // 记录图片真实的宽、高
 	Boundary bound;  // 图片的活跃区域，即正在矫正的区域
 
+	void setFileInfo(const char *);
+	void setImageInfo();
+	void doSave(const string &);
 };
 
